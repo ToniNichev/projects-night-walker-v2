6b32246a1cd7f86e24515a76e832b41b4de3d781
ToniNichev <toni.nichev@gmail.com>
2024-03-09 11:23:51 -0500

    work

--- node-app/public/single-leg/leg-todata.js
+++ node-app/public/single-leg/leg-todata.js
@@ -4,6 +4,8 @@ function addLegMovement() {
     var _scenes = ['sceneLeft', 'sceneRight'];
     var _legs = ['legOne', 'legTwo'];
     var _servos = ['servoOne', 'servoTwo'];
+
+    var dataField = document.getElementById('text-data');
     
     for(s = 0; s < 2; s++) {
         var _scene = _scenes[s];
@@ -14,7 +16,12 @@ function addLegMovement() {
             var _servo = _servos[i % 2];
 
             var angle = legs[_scene][_leg][_servo].angle;
-            console.log(angle);
+
+            if(i == 0) {
+                console.log(180 - angle * 2);
+            } else if (i ==1 ) {
+                console.log(180 + angle * 2);
+            }
         }
     }
 }
