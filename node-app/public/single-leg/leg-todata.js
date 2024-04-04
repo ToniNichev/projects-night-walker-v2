3a273413c48abe2c9d18e05dc2eb50e59a127d93
Toni Nichev <toni.nichev@gmai.com>
2024-04-04 18:09:09 -0400

    work

--- node-app/public/single-leg/leg-todata.js
+++ node-app/public/single-leg/leg-todata.js
@@ -3,38 +3,47 @@ function addLegMovement() {
 
     var _scenes = ['sceneLeft', 'sceneRight'];
     var _legs = ['legOne', 'legTwo'];
-    var _servos = ['servoOne', 'servoTwo'];
+    var _servos = ['servoOne', 'servoTwo',];
 
     var dataField = document.getElementById('text-data');
-    
-    for(s = 0; s < 2; s++) {
+
+    for (s = 0; s < 1; s++) {
         var _scene = _scenes[s];
-        for(var i=0;i < 4; i ++) {        
-            
+        for (var i = 0; i < 4; i++) {
+
             var _leg = _legs[Math.round(i / 4)];
-            
+
             var _servo = _servos[i % 2];
 
             var angle = legs[_scene][_leg][_servo].angle;
 
-            if(i == 0) {
-                console.log(180 - angle * 2);
-            } else if (i ==1 ) {
-                console.log(180 + angle * 2);
+            switch (i) {
+                case 0:
+                    console.log(180 - angle * 2);
+                    break;
+                case 1:
+                    console.log(180 + angle * 2);
+                    break;
+                case 2:
+                    console.log(180 - angle * 2);
+                    break;
+                case 3:
+                    console.log(180 + angle * 2);
+                    break;
             }
         }
     }
 }
 
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.metaKey || event.ctrlKey) {
         console.log('Command/Control key is down');
         commandDown = true;
     }
 });
 
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keyup', function (event) {
     if (!event.metaKey && !event.ctrlKey) {
         console.log('Command/Control key is up');
         commandDown = false;
